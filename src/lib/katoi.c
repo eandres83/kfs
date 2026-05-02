c40c342473eb8a8fbeb535527bec4d714bd7f9be
eandres <eandres@student.42urduliz.com>
2026-05-02 17:53:15 +0200

    add atoi for command like mount and umount that need a number :)

---
+++ src/lib/katoi.c
@@ -0,0 +1,24 @@
+#include <utils.h>
+
+size_t	katoi(char *str)
+{
+	char *tmp = str;
+	int result = 0;
+	int sign = 1;
+
+	while (*tmp == 32 || (*tmp >= 9 && *tmp <= 13))
+		tmp++;
+	if (*tmp == '-' || *tmp == '+')
+	{
+		if (*tmp == '-')
+			sign *= -1;
+		tmp++;
+	}
+	while (*tmp >= '0' && *tmp <= '9')
+	{
+		result = result * 10 + *tmp - '0';
+		tmp++;
+	}
+	return (result * sign);
+}
+
